a5628cb5a9aed5ae6da3a656fb407986e7861aef
unknown <chanzobryan@gmail.com>
2020-09-11 00:27:31 -0400

    Update env to use new vision module and better doc string

--- speedrunnersai/speedrunners/sr_env.py
+++ speedrunnersai/speedrunners/sr_env.py
@@ -3,14 +3,18 @@ import d3dshot
 
 from time import time
 from pymem import Pymem
-from torch.nn.functional import interpolate
+from typing import Tuple, Optional
+from hlrl.core.vision import FrameHandler
+from hlrl.core.vision.transforms import Grayscale, Interpolate
 
-#from speedrunnersai.speedrunners.screen_viewer import ScreenViewer
 from speedrunnersai.speedrunners.memory_reader import MemoryReader
 from speedrunnersai.speedrunners.actor import Actor
 from speedrunnersai.speedrunners.structures import Address, Player, Match
 
 class SpeedRunnersEnv():
+    """
+    The SpeedRunners environment to be interacted with.
+    """
     # The addresses of all the values to get
     addresses = {"x_speed" : 0x29CBF23C,
                  "y_speed" : 0x29CBF240,
@@ -20,23 +24,33 @@ class SpeedRunnersEnv():
     # The offsets of all the values to get
     offsets = {"obstacles_hit" : [0x0, 0x0, 0x1F6C, 0x8, 0x4]}
 
-    def __init__(self, max_time, window_size, res_shape, read_mem = False):
+    def __init__(self, max_time: float, res_shape: Tuple[int, int, int],
+        window_size: Optional[Tuple[int, int, int, int]] = None,
+        read_mem: bool = False):
+        """
+        Creates the environment.
+
+        Args:
+            max_time (float): The maximum amount of time an agent can play
+                before an environment reset.
+            res_shape (Tuple[int, int, int]): The shape to resize the captured
+                images to. If the channel dimension is 1 or less, converts to
+                grayscale.
+            window_size (Optional[Tuple[int, int, int, int]]): The values of
+                (left, top, right, bottom) to capture.
+            read_mem (bool): If true, will attach to the speedrunners process to
+                read memory values (DO NOT USE ONLINE).
+        """
         self.res_shape = res_shape
         self.actor = Actor()
 
         # Create the d3dshot instance
-        d3dshot = d3dshot.create(capture_output="pytorch_float_gpu")
-        formula = torch.FloatTensor([0.2125, 0.7154, 0.0721]).to("cuda")
-        grayscale_transform = lambda frame: do_mul(frame, formula).unsqueeze(1)
-        interpolate_transform = lambda frame: interpolate(
-            frame, size=(128, 128), mode="bilinear"
-        )
-        
-        transforms = [grayscale_transform]#, interpolate_transform]
-        capture_time = 10
-        frame_handler = FrameHandler(d3d, transforms=transforms)
+        d3d = d3dshot.create(capture_output="pytorch_float_gpu")
 
-        #self.sv = ScreenViewer(window_size, res_shape, self._screen_update_hook)
+        transforms = [Grayscale()] if res_shape[-1] <= 1 else []
+        transforms.append(Interpolate(size=res_shape[:-1], mode="bilinear"))
+
+        self.frame_handler = FrameHandler(d3d, transforms=transforms)
         
         self.max_time = max_time
         self.num_actions = self.actor.num_actions()
@@ -93,8 +107,7 @@ class SpeedRunnersEnv():
         """
         self.actor.reset()
         self._episode_finished(True)
-        self.sv.set_polled()
-        self.sv.GetNewScreen()
+        self.frame_handler.get_new_frame()
         self.start_time = time()
 
         return self.state
@@ -103,7 +116,7 @@ class SpeedRunnersEnv():
         """
         Starts the screen viewer.
         """
-        self.sv.Start()
+        self.frame_handler.capture(target_fps=240)
 
         if self.memory is not None:
             self.memory.open_process_from_name("speedrunners.exe")
@@ -112,14 +125,14 @@ class SpeedRunnersEnv():
         """
         Closes the screen viewer.
         """
-        self.sv.Stop()
+        self.frame_handler.stop()
 
     def stop(self):
         """
         Closes the environment.
         """
         self.reset()
-        self.sv.Stop()
+        self.frame_handler.stop()
         self.actor.stop()
 
         if self.memory is not None:
@@ -129,9 +142,9 @@ class SpeedRunnersEnv():
         """
         Takes a step into the environment with the action given.
         """
-        self.sv.set_polled()
         self.actor.act(action)
-        self._state = self.sv.GetNewScreen()
+        self.frame_handler.get_new_frame()
+        self._state = self.frame_handler.get_frame_stack([0], "first")
 
         # Update structures with new memory
         self._update_memory()
